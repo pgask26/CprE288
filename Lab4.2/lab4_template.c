bd6d1de9b06dd09915213553b0e8eb93721aa742
pgask26 <132099295+pgask26@users.noreply.github.com>
2025-02-11 10:41:37 -0600

    Add files via upload

---
+++ Lab4.2/lab4_template.c
@@ -0,0 +1,42 @@
+/**
+ * lab4_template.c
+ *
+ * Template file for CprE 288 lab 4
+ *
+ * @author Zhao Zhang, Chad Nelson, Zachary Glanz
+ * @date 08/14/2016
+ */
+
+#include "button.h"
+#include "Timer.h"
+#include "lcd.h"
+#include "cyBot_uart.h"  // Functions for communicating between CyBot and Putty (via UART)
+// PuTTy: Baud=115200, 8 data bits, No Flow Control, No Parity, COM1
+
+#define REPLACEME 0
+
+
+
+int main(void) {
+    button_init();
+    timer_init(); // Must be called before lcd_init(), which uses timer functions
+    lcd_init();
+    // Don't forget to initialize the cyBot UART before trying to use it
+    cyBot_uart_init();
+
+    // YOUR CODE HERE
+    uint8_t number = 0;
+
+    while(1)
+    {
+
+        // YOUR CODE HERE
+        char print[20];
+        number = button_getButton();
+        sprintf(print, "%d", number);
+        lcd_printf(print);
+        cyBot_sendByte(print[0]);
+    }
+
+
+}
---
+++ lab4/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LIB_SRCS += \
+../libcybotUART.lib 
+
+C_SRCS += \
+../Timer.c \
+../button.c \
+../lab4_template.c \
+../lcd.c 
+
+C_DEPS += \
+./Timer.d \
+./button.d \
+./lab4_template.d \
+./lcd.d 
+
+OBJS += \
+./Timer.obj \
+./button.obj \
+./lab4_template.obj \
+./lcd.obj 
+
+OBJS__QUOTED += \
+"Timer.obj" \
+"button.obj" \
+"lab4_template.obj" \
+"lcd.obj" 
+
+C_DEPS__QUOTED += \
+"Timer.d" \
+"button.d" \
+"lab4_template.d" \
+"lcd.d" 
+
+C_SRCS__QUOTED += \
+"../Timer.c" \
+"../button.c" \
+"../lab4_template.c" \
+"../lcd.c" 
+
+
